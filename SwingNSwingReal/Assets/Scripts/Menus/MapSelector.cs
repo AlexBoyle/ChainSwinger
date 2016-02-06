4f087dcd1a5ea60be2034535ab5ff3b97f57b88b
Alex Boyle <aboyle75@gmail.com>
2016-02-06 17:34:00 -0600

    99% done with level select, todo: add select animation?

--- SwingNSwingReal/Assets/Scripts/Menus/MapSelector.cs
+++ SwingNSwingReal/Assets/Scripts/Menus/MapSelector.cs
@@ -4,6 +4,7 @@ using XInputDotNetPure;
 public class MapSelector : MonoBehaviour {
 	public int playerNumber;
 	private int playerX = 0;
+	private bool selected = false;
 	private int playerY = 0;
 	private float moveX;
 	private float moveY;
@@ -28,22 +29,30 @@ public class MapSelector : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		state = GamePad.GetState (playerIndex, GamePadDeadZone.None);
-		if (moveCoolDownVar < 0) {
-			moveCoolDownVar = moveCoolDown;
-			//
-			if (state.ThumbSticks.Left.X > .4f) {
-				if (mapX -1 > playerX)
-					playerX ++;
-			} else if (state.ThumbSticks.Left.X < -.4f) {
-				if(0 < playerX)
-					playerX --;
+		if(!selected)
+		{
+			if (state.Buttons.A == ButtonState.Pressed) {
+				selected = true;
+
+				mapDim.selectMap (playerX + (playerY * mapX));
 			}
-			if (state.ThumbSticks.Left.Y < -.4f) {
-				if (mapY -1 > playerY)//idk this wokrs now
-					playerY ++;
-			} else if (state.ThumbSticks.Left.Y > .4f) {
-				if(0 < playerY)
-					playerY --;
+			if (moveCoolDownVar < 0) {
+				moveCoolDownVar = moveCoolDown;
+				//
+				if (state.ThumbSticks.Left.X > .4f) {
+					if (mapX - 1 > playerX)
+						playerX++;
+				} else if (state.ThumbSticks.Left.X < -.4f) {
+					if (0 < playerX)
+						playerX--;
+				}
+				if (state.ThumbSticks.Left.Y < -.4f) {
+					if (mapY - 1 > playerY)//idk this wokrs now
+					playerY++;
+				} else if (state.ThumbSticks.Left.Y > .4f) {
+					if (0 < playerY)
+						playerY--;
+				}
 			}
 		}
 		gameObject.GetComponent<Transform>().position = Vector3.Lerp(gameObject.GetComponent<Transform>().position, new Vector3((playerX * moveX)-10.8f +(playerNumber * .9f),(playerY * -moveY) + 1,0), .2f);
--- SwingNSwingReal/Assets/Scripts/Menus/Maps.cs
+++ SwingNSwingReal/Assets/Scripts/Menus/Maps.cs
@@ -1,29 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 public class Maps : MonoBehaviour {
+
+	public int firstMapIndex = 0;
 	public GameObject player;
-	public int test;
+	private int[] mapVotes;
+	public int rows = 2;
 	public GameObject[] mapImages;
-	private int numPlayers = 4;
+	public int numPlayers = 4;
+	private int votes = 0;
 	private int width;
 	private int height;
 	public float disX = 4f;
 	public float disY = 3f;
 	// Use this for initialization
-	void Start () {
-		width = mapImages.Length/2;
-		height = 2;
+	void Start () 
+	{
+		//sets the width and height of the map grid off of the number of rows and number of maps
+		mapVotes = new int[mapImages.Length];
+		for (int i = 0; i < mapVotes.Length; i++)
+			mapVotes[i] = 0;
+		width = mapImages.Length/rows;
+		height = rows;
 		int j = -1;
-		Debug.Log (numPlayers);
+		//adds all the players in
 		for (int i = 0; i < numPlayers; i++) {
-			Debug.Log ("spawn");
 			(Instantiate (player) as GameObject).GetComponent<MapSelector> ().setPlayerNum (i);
 		}
+		//displays every map
 		for (int i = 0; i < mapImages.Length; i++) {
 			if (i % width == 0)
 				j++;
 			Instantiate(mapImages[i],new Vector3(((i%width) * disX)-9, (j * -disY) +3, 0),Quaternion.identity);
+		}
+	}
+	//collects map voats and then picks the next level
+	public void selectMap(int selection)
+	{
+		
+		mapVotes [selection] ++;
+		votes++;
+		//the voting thing
+		if (votes == numPlayers)
+		{
+			List<int> mapToPlay = new List<int>();
+			int level = 0;
+			mapToPlay.Add (-1);
+			//check the votes for each level
+			for (int i = 0; i < mapVotes.Length; i++) {
+				if (mapVotes [i] == mapVotes [level]) {
+					mapToPlay.Add (i);
+					level = i;
+				} else if (mapVotes [i] > mapVotes [level]) {
+					mapToPlay.Clear ();
+					level = i;
+					mapToPlay.Add (i);
+				}
+			}
+			Debug.Log (mapToPlay.Count);
+			//pick level
+			if (mapToPlay.Count <= 1) {
+				SceneManager.LoadScene(mapToPlay [0] + firstMapIndex);
+			} else {
+				Random.seed = System.DateTime.Now.Millisecond;
+				SceneManager.LoadScene(mapToPlay[(int)(Random.value * 100) %mapToPlay.Count] + firstMapIndex);
+			}
 		}
 	}
 	public int getWidth()
