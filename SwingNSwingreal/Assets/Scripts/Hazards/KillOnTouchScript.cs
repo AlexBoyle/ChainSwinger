38ab8e811ab6246aaae0d615e73d6f59a3fa966a
Alex Boyle <aboyle75@gmail.com>
2016-02-15 19:32:32 -0600

    k

---
+++ SwingNSwingreal/Assets/Scripts/Hazards/KillOnTouchScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillOnTouchScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			
+			other.GetComponent<HealthScript> ().DealDamage (100, -1, true);
+		}
+	}
+}
