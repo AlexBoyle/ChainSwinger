0994c1215cd381a44df2489757e7716cd50be0d2
AlexBoyle <aboyle75@gmail.com>
2016-03-14 13:42:17 -0500

    some changes to Hazards

--- SwingNSwingReal/Assets/Scripts/Hazards/CutChain.cs
+++ SwingNSwingReal/Assets/Scripts/Hazards/CutChain.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class CutChain : MonoBehaviour {
 	GameObject ChainHitbox;
+	public bool On = true;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	void OnTriggerEnter2D(Collider2D other){
-		//Debug.Log (other.tag);
-		if(other.tag == "Chain" ){
-			other.GetComponent<ChainDestructionScript> ().DestroyChain (transform.position);
+		if (On) {
+			if (other.tag == "Chain") {
+				other.GetComponent<ChainDestructionScript> ().DestroyChain (transform.position);
+			}
 		}
 	}
 
