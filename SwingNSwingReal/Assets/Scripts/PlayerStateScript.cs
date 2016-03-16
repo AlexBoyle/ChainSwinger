e9115333ce97382755280c1e36c89e708973584a
CaptKonrad <robkondratowicz@gmail.com>
2016-03-16 12:03:13 -0500

    Began Animation Implementation
    Created the Files necessary for the Animations

---
+++ SwingNSwingReal/Assets/Scripts/PlayerStateScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStateScript : MonoBehaviour
+{
+	//0 = on Ground
+	//1 = in Air
+	//2 = on Wall
+	private int State;
+	private bool FacingRight;
+
+	public Animator anim;
+
+	void Start ()
+	{
+		anim = gameObject.GetComponent<Animator>();
+		State = 0;
+		FacingRight = false;
+	}
+
+	void Update ()
+	{
+		UpdateAnimations();
+	}
+
+	private void UpdateAnimations()
+	{
+		anim.SetBool("FacingRight", FacingRight);
+	}
+
+	public void SetFacingRight(bool facer)
+	{
+		FacingRight = facer;
+	}
+
+	public void UpdateState(int newstate)
+	{
+		State = newstate;
+	}
+}
